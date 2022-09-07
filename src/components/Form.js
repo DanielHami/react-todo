bf03d5ea3e997cb838ee91db15d1ddd62c235d05
unknown <danihami44@gmail.com>
2022-09-07 23:10:56 +0200

    I applicated a supbase database instead of my json file

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,42 +1,46 @@
 import { useState } from "react"
-import axios from "axios"
+import { supabase } from "../supabaseClient"
+/*import TodoList from "./TodoList"*/
+import { BsPlusCircle } from 'react-icons/bs'
 import TodoList from "./TodoList"
-import {BsPlusCircle} from 'react-icons/bs'
+
+
 
 function Form() {
     const [title, setTitle] = useState('')
+    const [formError, setFormError] = useState(null)
     const [status, setStatus] = useState(false)
 
 
 
-
-    const HandleItem = (e) => {
-      
-        axios.post('http://localhost:8000/todos', {
-            title
-        })
-            .then(function (response) {
-                
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+    const HandleItem = async (e) => {
+        e.preventDefault();
+        if (!title) {
+            setFormError('Please fill in the field correctly')
+            return
+        }
+        const { data, error } = await supabase
+            .from('todo')
+            .insert({ title })
+        if (data)
+           setFormError(null)
+           window.location.reload()
     }
-  
-  
 
 
     return (
         <div className="mb-5 max-w-7xl mx-auto ">
-        <div className="py-20 text-center space-y-12 px-2">
-            <h1 className="text-4xl md:text-6xl font-semibold">Cl!ck the plus add your task</h1>
-            <button type="submit" onClick={() => setStatus(!status)}><BsPlusCircle className="text-4xl mt-6 mb-6 "/></button>
-            {status ? <form  onSubmit={HandleItem} className="space-x-2">
-                <input type="text" placeholder="Enter your task" value={title} onChange={e => setTitle(e.target.value)} className=" rounded-xl p-2 w-full md:w-96"></input>
-                <button type="submit" className="s w-1/2 md:w-32 mt-5 py-2 bg-black text-white rounded-xl">OK</button>
-            </form> : null }
-        </div>
-        <TodoList/>
+            <div className="py-20 text-center space-y-12 px-2">
+                <h1 className="text-4xl md:text-6xl font-semibold">Cl!ck the plus add your task</h1>
+                <button type="submit" onClick={() => setStatus(!status)}><BsPlusCircle className="text-4xl mt-6 mb-6 " /></button>
+                {status ? <form onSubmit={HandleItem} className="space-x-2">
+                    <input type="text" placeholder="Enter your task" value={title} onChange={e => setTitle(e.target.value)} className=" rounded-xl p-2 w-full md:w-96"></input>
+                    <button type="submit" className="s w-1/2 md:w-32 mt-5 py-2 bg-black text-white rounded-xl">OK</button>
+                </form> : null}
+                {formError && <p>{formError}</p>}
+            </div>
+            <TodoList />
+
         </div>
     )
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,64 +1,79 @@
+import { supabase } from "../supabaseClient"
 import { useEffect, useState } from "react"
-import axios from "axios"
 import { BsCheckCircle } from 'react-icons/bs'
 import { StyledButton, StyledText } from "./style/StyledTodo"
 
 const TodoList = () => {
+    const [fetchError, setFechError] = useState(null)
     const [items, setItems] = useState([])
     const [state, setState] = useState('')
-    const [newText, setNewText] = useState('')
+    const [title, setTitle] = useState('')
+
+    async function handlePut(submitId) {
+        const {data, error } = await supabase
+            .from('todo')
+            .update({ title: `${title}`, state: 'none' })
+            .eq('id', submitId)
+            window.location.reload()
+    }
 
     useEffect(() => {
-        axios.get('http://localhost:8000/todos', {
-        })
-            .then(function (response) {
-                setItems(response.data);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+        async function fetchData() {
+            let { data, error } = await supabase
+                .from('todo')
+                .select('*')
+            setItems(data)
+            if (error){
+                setFechError('Could not fetch the todos')
+                setItems(null)
+                alert(fetchError)
+            }
+            if (data) {
+                setFechError(null)
+            }
+        }
+        fetchData()
     }, [])
 
-
-    function HandleDelete(parameter) {
-        axios.delete(`http://localhost:8000/todos/${parameter}`)
-        window.location.reload(false);
+    async function HandleDelete(parameter) {
+        const { } = await supabase
+            .from('todo')
+            .delete()
+            .eq('id', parameter)
+            window.location.reload()
     }
 
 
-    function handlePut(submitId, title) {
-        const article = { title: `${newText}` };
-        axios.put(`http://localhost:8000/todos/${submitId}`, article)
 
-    }
 
-    function handleStatus(id, fax) {
-        axios.put(`http://localhost:8000/todos/${id}`, {
-            "title": `${fax}`,
-            "state": "done"
-        })
-
+   async function handleStatus(id) {
+        const { data, error } = await supabase
+            .from('todo')
+            .update({
+                 state: 'done',})
+            .eq('id', id)
+            window.location.reload()
     }
 
 
 
     function changeIsReply(clickedId, title) {
-        setNewText(title)
+        setTitle(title)
         setState(clickedId)
 
     }
-
+    console.log(title)
 
     return (
         <div className="flex flex-col gap-6 mt-20 lg:grid lg:grid-cols-4 md:grid md:grid-cols-3  md:gap-5">
             {items.map(props =>
                 <div key={props.id} className="flex flex-col gap-8 justify-around rounded-lg text-left px-4 py-6 space-y-5 backdrop-blur-sm bg-white/30 ">
-                    {state === props.id ? <div className="w-full"><input value={newText} name={props.title} placeholder={props.title} onChange={(e) => setNewText(e.target.value)} className="border-2 rounded-xl px-2"></input></div>
-                        : <div><div className="flex gap-2 items-center"><StyledButton prop={props.state} onClick={() => { handleStatus(props.id, props.title); window.location.reload() }}><BsCheckCircle className="text-xl my-auto hover:text-green-300" /></StyledButton> {props.state && <p className="">Done</p>} </div> <StyledText prop={props.state}>{props.title}</StyledText></div>}
+                    {state === props.id ? <div className="w-full"><input value={title} name={props.title} placeholder={props.title} onChange={(e) => setTitle(e.target.value)} className="border-2 rounded-xl px-2"></input></div>
+                        : <div><div className="flex gap-2 items-center"><StyledButton prop={props.state} onClick={() => { handleStatus(props.id, props.title) }}><BsCheckCircle className="text-xl my-auto hover:text-green-300" /></StyledButton> {props.state && <p className="">Done</p>} </div> <StyledText prop={props.state}>{props.title}</StyledText></div>}
                     <div>
                         <button onClick={() => HandleDelete(props.id)} className="border-2 border-red-500 w-20 rounded-xl mr-2" >Delete</button>
                         {state === props.id ?
-                            <button onClick={() => { handlePut(props.id, props.title); window.location.reload() }} className="border-2 w-20 border-blue-900 rounded-xl">Submit</button> :
+                            <button onClick={() => handlePut(props.id, props.title)} className="border-2 w-20 border-blue-900 rounded-xl">Submit</button> :
                             <button onClick={() => changeIsReply(props.id, props.title)} className="border-2 border-blue-900 w-20 rounded-xl">Edit</button>}
                     </div>
                 </div>)}
